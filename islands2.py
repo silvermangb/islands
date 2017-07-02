83628a44ce6cd639f9abd8d1be4e10a23ddc721a
Greg Silverman <greg.silverman@memeo-inc.com>
2017-07-02 10:33:11 -0700

    New input data generators 0

--- islands2.py
+++ islands2.py
@@ -1,5 +1,4 @@
-import pprint
-
+import random
 '''
 000000
 011110
@@ -8,13 +7,17 @@ import pprint
 000000
 '''
 
+max_stack = 0
 
 def traverse(p_map, p_rows, p_cols, p_island_color, ):
+    global max_stack
+
     loc = [-1, -1]
 
     stack = []
 
     while loc[0] + 1 < p_rows:
+
         loc[0] += 1
         loc[1] = -1
         while loc[1] + 1 < p_cols:
@@ -23,6 +26,8 @@ def traverse(p_map, p_rows, p_cols, p_island_color, ):
                 continue
             stack.append((loc[0], loc[1], p_island_color))
             while stack:
+                if len(stack) > max_stack:
+                    max_stack = len(stack)
                 top = stack[-1]
                 if p_map[top[0]][top[1]] == 0:
                     stack.pop()
@@ -54,28 +59,37 @@ def traverse(p_map, p_rows, p_cols, p_island_color, ):
     return p_island_color
 
 
-rows = 8
-cols = 8
+rows = 1024
+cols = 1024
 input_data = [[0 for j in xrange(cols)] for i in xrange(rows)]
 
-input_data[0] = [1 for i in xrange(cols)]
-input_data[rows - 1] = [1 for i in xrange(cols)]
-for i in xrange(rows):
-    input_data[i][0] = 1
-    input_data[i][cols - 1] = 1
 
-for r in xrange(3, 6):
-    for c in xrange(3, 6):
-        input_data[r][c] = 1
-input_data[2][2] = 1
+def data1(p_rows, p_cols, p_input_data):
+    p_input_data[0] = [1 for i in xrange(p_cols)]
+    p_input_data[rows - 1] = [1 for i in xrange(p_cols)]
+    for i in xrange(p_rows):
+        p_input_data[i][0] = 1
+        p_input_data[i][p_cols - 1] = 1
 
-pprint.pprint(input_data)
+    for r in xrange(3, 6):
+        for c in xrange(3, 6):
+            p_input_data[r][c] = 1
+    p_input_data[2][2] = 1
 
-island_color = 1
+
+def data2(p_rows, p_cols, p_input_data, p_percent):
+    cell_count = p_rows * p_cols
+    ones = int(p_percent * cell_count)
+    cells = [(i, j) for j in xrange(p_cols) for i in xrange(p_rows)]
+    land_cells = random.sample(cells, ones)
+    for p in land_cells:
+        p_input_data[p[0]][p[1]] = 1
+
+
+data2(rows, cols, input_data, .75)
+
 
 island_count = traverse(input_data, rows, cols, 1) - 1
 
-for row in input_data:
-    row = ['%02d' % i for i in row]
-    print str(row)
 print island_count
+print max_stack
